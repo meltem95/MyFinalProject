ca5db9febf65a7df5611afcb9deaa6fcd254ec9f
Meltem Akdenizli <meltem1995.ma@gmail.com>
2021-02-11 16:24:22 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity implement eden class class bir veritabı tablosudur.
+    public interface IEntity
+    {
+    }
+}
