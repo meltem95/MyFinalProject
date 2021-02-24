fcb0f0702f817cf9d2d38ab48eb3c2a75557880d
Meltem Akdenizli <meltem1995.ma@gmail.com>
2021-02-24 23:46:50 +0300

    6 Şubat

--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     //IEntity implement eden class class bir veritabı tablosudur.
     public interface IEntity
